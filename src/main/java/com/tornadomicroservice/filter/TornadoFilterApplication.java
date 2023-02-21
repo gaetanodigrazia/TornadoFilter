2d805ce09c5d21b7cb730ccbd5637def9c8b2e24
Gaetano Di Grazia <107346930+gaetanodigrazia@users.noreply.github.com>
2023-02-21 22:29:23 +0100

    Initial commit

---
+++ src/main/java/com/tornadomicroservice/filter/TornadoFilterApplication.java
@@ -0,0 +1,13 @@
+package com.tornadomicroservice.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornadoFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornadoFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tornadomicroservice/filter/TornadoFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornadomicroservice.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornadoFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
